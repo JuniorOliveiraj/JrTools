1056e445881d79de59c7475570f6eddd8b3eaf08
junior <junioroliveira.belem@gmail.com>
2025-08-31 23:22:23 -0300

    Adicionar arquivos de projeto.

---
+++ JrTools/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace JrTools
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ContentFrame.Navigate(typeof(Pages.HomePage));
+
+        }
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.SelectedItem is NavigationViewItem item)
+            {
+                switch (item.Tag)
+                {
+                    case "home":
+                        ContentFrame.Navigate(typeof(Pages.HomePage));
+                        break;
+                    case "library":
+                        ContentFrame.Navigate(typeof(Pages.LibraryPage));
+                        break;
+                    case "downloads":
+                        ContentFrame.Navigate(typeof(Pages.DownloadsPage));
+                        break;
+                }
+            }
+        }
+
+
+    }
+}
